ac25e24ad0d7a092565e467e4f6b264d6a13989d
esojogerba <esojogerba@users.noreply.github.com>
2023-07-16 16:15:39 -0500

    Cleaned up global.js.

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,13 +1,12 @@
 'use strict';
 
-// Add event on multiple elements
-
+// Adds event on multiple elements.
 const addEventOnElements = function (elements, eventType, callback) {
     for (const elem of elements) elem.addEventListener(eventType, callback);
 }
 
-//  Toggle search box in mobile device || small screen
 
+//  Toggle search box in mobile screens.
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
 
