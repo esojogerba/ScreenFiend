14c1df281008c476905088a2c785c79b70312ee8
esojogerba <esojogerba@users.noreply.github.com>
2023-08-09 21:59:19 -0500

    Implemented sidebar in shows page.

--- assets/js/shows.js
+++ assets/js/shows.js
@@ -1,8 +1,12 @@
 "use strict";
 
 // Imports
+import { sidebar } from "./sidebar.js";
 import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
 import { createShowCard } from "./show-card.js";
+
+// Sidebar functionality.
+sidebar("tv");
 
 // Retrieves page-content <article> from index page.
 const pageContent = document.querySelector("[page-content]");
